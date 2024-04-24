a024ac0023f14687b2f674946a8288f62f2ef50d
Mehboob Shaikh <ms568203@gmail.com>
2024-04-24 12:46:59 +0530

    updated database in settings

--- plagarism_checker/settings.py
+++ plagarism_checker/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'plagarism_checker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'test',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',   # Or the host where your MySQL server is running
-#         'PORT': '3306',        # Default MySQL port
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or the host where your MySQL server is running
+        'PORT': '3306',        # Default MySQL port
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
